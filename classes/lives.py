69b79e3af6d7efd88ff620d89913dc4a72be43bd
Leshawn Rice <leshawn.rice@outlook.com>
2021-11-20 15:27:37 -0800

    updated lives to use symbols

--- classes/lives.py
+++ classes/lives.py
@@ -4,31 +4,31 @@ import pygame
 
 
 class Lives:
-    def __init__(self, lives: int = 5, x_position: int = 13, y_position: int = 13, size: int = 25):
+    def __init__(self, lives: int = 5, x_position: int = 5, y_position: int = 0, size: int = 25):
         self.lives = lives
         self.max_lives = lives
         self.size = size
         self.x_position = x_position
         self.y_position = y_position
         self.position = (self.x_position, self.y_position)
-        self.color = COLORS.get('WHITE')
+        self.color = COLORS.get('RED')
         self.background_color = COLORS.get('BLUE')
-        self.font = pygame.font.Font("freesansbold.ttf", self.size)
-        self.text = '<3 ' * self.lives + '</3' * \
+        self.font = pygame.font.Font("seguisym.ttf", self.size)
+        self.text = '♥ ' * self.lives + '♡ ' * \
             (0 + (self.max_lives - self.lives))
         self.sprite = self.font.render(
             self.text, True, self.color, self.background_color)
 
     def remove(self):
         self.lives -= 1
-        self.text = '<3 ' * self.lives + '</3' * \
+        self.text = '♥ ' * self.lives + '♡ ' * \
             (0 + (self.max_lives - self.lives))
         self.sprite = self.font.render(
             self.text, True, self.color, self.background_color)
 
     def add(self):
         self.lives += 1
-        self.text = '<3 ' * self.lives + '</3' * \
+        self.text = '♥ ' * self.lives + '♡ ' * \
             (0 + (self.max_lives - self.lives))
         self.sprite = self.font.render(
             self.text, True, self.color, self.background_color)
